11ae3a64517be3bb335fdd95dee19be7b2dca28b
hendrosetwn <hendro.setiawan1995@gmail.com>
2022-09-18 10:18:26 +0700

    add service method

---
+++ src/app.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { data, ReportType } from './data';
+import { AppService } from './app.service';
+
+@Controller('report/:type')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return this.appService.getAllReports(reportType);
+  }
+
+  @Get(':id')
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return this.appService.getReportById(reportType, id);
+  }
+
+  @Post()
+  createReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: string,
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.appService.createReport(reportType, { amount, source });
+  }
+
+  @Put(':id')
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { amount: number; source: string },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return this.appService.updateReport(reportType, id, body);
+  }
+
+  @Delete(':id')
+  deleteReport(@Param('id') id: string) {
+    return this.appService.deleteReport(id);
+  }
+}
